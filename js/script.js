b3bb84287f36d0c8a0109f0c9402cfff7a51f1ab
Stepan <empo999pr@gmail.com>
2020-04-19 19:06:38 +0300

    Added html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function init () {
+  const $form = document.getElementById('survey-form');
 
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    alert('Submitted');
+  });
 }
 
 init();
