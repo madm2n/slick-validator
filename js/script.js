b4e6d8506bfe6fa290143cd07512feab83bdb033
Stepan <empo999pr@gmail.com>
2020-04-19 20:52:03 +0300

    Select update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+const ACTIVE_CLASS = 'active';
+
 function init () {
   const $form = document.getElementById('survey-form');
+  const $fields = document.getElementsByClassName('field');
+
+  function toggle($input, $field) {
+    return () => {
+      if ($input.value) {
+        $field.classList.add(ACTIVE_CLASS);
+      } else {
+        $field.classList.toggle(ACTIVE_CLASS);
+      }
+    }
+  }
+
+  for (const $field of $fields) {
+    const $input = $field.querySelector('input, textarea, select');
+    const listener = toggle($input, $field);
+
+    $input.addEventListener('focus', listener);
+    $input.addEventListener('focusout', listener);
+
+    if ($input.value) {
+      listener();
+    }
+  }
 
   $form.addEventListener('submit', (event) => {
     event.preventDefault();
